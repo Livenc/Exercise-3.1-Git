448f85831c84cb974e5613134143c9329cf9360b
Artem Alimov <artemalimov33@gmail.com>
2023-02-11 22:02:46 +0200

    Add 5 files

---
+++ Exercise 3.1 Git/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3._1_Git
+{
+    internal class Class1
+    {
+    }
+}
